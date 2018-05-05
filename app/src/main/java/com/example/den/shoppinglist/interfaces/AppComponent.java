63d2c45a8ebd277dfefe329517229ca4525ad160
den <stoplus@gmail.com>
2018-05-05 21:06:16 +0300

    сделал первичную разметку активности покупок

--- app/src/main/java/com/example/den/shoppinglist/interfaces/AppComponent.java
+++ app/src/main/java/com/example/den/shoppinglist/interfaces/AppComponent.java
@@ -1,7 +1,6 @@
 package com.example.den.shoppinglist.interfaces;
 
 import com.example.den.shoppinglist.MainActivity;
-import com.example.den.shoppinglist.module.DatabaseModule;
 import com.example.den.shoppinglist.module.RequestsListsModule;
 
 import javax.inject.Singleton;
